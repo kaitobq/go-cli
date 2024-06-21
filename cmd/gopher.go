ee67f654c66bd5919b302f53f643130f9f1dce69
kaitobq <kaitobq520@gmail.com>
2024-06-21 16:32:11 +0900

    done

--- cmd/gopher.go
+++ cmd/gopher.go
@@ -6,8 +6,13 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
+	"syscall"
+	"time"
 
+	"github.com/gosuri/uilive"
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 // gopherCmd represents the gopher command
@@ -15,12 +20,51 @@ var gopherCmd = &cobra.Command{
 	Use:   "gopher",
 	Short: "Print the ASCII art of a gopher",
 	Run: func(cmd *cobra.Command, args []string) {
-		b, err := os.ReadFile("aa/gopher.txt")
-		if err != nil {
-			fmt.Println("Error reading file:", err)
-			return
+		b := []string{
+			"         ,_---~~~~~----._         ",
+			"  _,,_,*^____      _____``*g*\"*, ",
+			" / __/ /'     ^.  /      \\ ^@q   f ",
+			"[  @f | @))    |  | @))   l  0 _/  ",
+			" \\`/   \\~____ / __ \\_____/    \\   ",
+			"  |           _l__l_           I   ",
+			"  }          [______]           I  ",
+			"  ]            | | |            |  ",
+			"  ]             ~ ~             |  ",
+			"  |                            |   ",
+			"   |                           | ",
 		}
-		fmt.Println(string(b))
+
+		bw := len(b[0])
+
+		tw, _, err := terminal.GetSize(syscall.Stdin)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		for i := range b {
+			b[i] = strings.Repeat(" ", tw) + b[i]
+		}
+	
+		writer := uilive.New()
+		
+		writer.Start()
+		for i := 0; i <= tw+bw; i++ {
+			var out string
+			for _, v := range b {
+				if i < bw {
+					out += v[i:tw+i] + "\n"
+					} else if i < len(v) {
+						out += v[i:] + "\n"
+						} else {
+							out += "\n"
+						}
+					}
+					fmt.Fprint(writer, out)
+					time.Sleep(10 * time.Millisecond)
+				}
+			writer.Stop()
+		// fmt.Println(strings.Join(b, "\n"))
 	},
 }
 
